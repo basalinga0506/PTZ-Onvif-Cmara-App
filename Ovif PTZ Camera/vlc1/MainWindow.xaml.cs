d693a6e9f81ae9c5356aa7ad4939efd01d26f303
basalinga0506 <basalinga0506@gmail.com>
2021-12-05 00:13:06 +0530

    First Time Upload Onvif PTZ Project

---
+++ Ovif PTZ Camera/vlc1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vlc.DotNet.Wpf;
+
+namespace vlc1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public VlcControl myVlcControl;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myVlcControl.MediaPlayer.VlcLibDirectory =
+                //replace this path with an appropriate one
+            new DirectoryInfo(@"c:\Program Files (x86)\VideoLAN\VLC\");
+            myVlcControl.MediaPlayer.EndInit();
+            myVlcControl.MediaPlayer.Play(new Uri("http://download.blender.org/peach/" +
+                "bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
+        }
+    }
+}
